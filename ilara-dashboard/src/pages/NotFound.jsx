085e06e076de493cc12087fd4331e2c0a967b16b
Phylis <chumba.phyl@gmail.com>
2021-12-18 20:38:32 +0300

    update not found message

--- ilara-dashboard/src/pages/NotFound.jsx
+++ ilara-dashboard/src/pages/NotFound.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 const NotFound = () => {
   return (
     <div className="app" style={{ flexDirection: 'column' }}>
-      <p>You took a wrong turn, mate.</p>
+      <p>
+        Sorry, no data found.
+        <b />
+        Try later
+      </p>
     </div>
   );
 };
